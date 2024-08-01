64f21ddba3d2d17c28105b27aa6d6d0467f820e5
Bhavishya khemnani <bobysoni9636@gmail.com>
2024-08-01 16:43:48 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+if(bar){
+    bar.addEventListener("click",()=>{
+        nav.classList.add("active");
+    })
+}
+if(close){
+    bar.addEventListener("click",()=>{
+        nav.classList.remove("active");
+    })
+}
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    $('#name').text(profile.getname());
+    $('#email').text(profile.getemail());
+    $("#img").attr('src',profile.getImageUrl());
+    $(".data").css("display","block");
+    $(".g-signin2").css("display","none");
+}
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
+      $(".g-signin2").css("display","block");
+      $(".data").css("display","none");
+    });
+  }
